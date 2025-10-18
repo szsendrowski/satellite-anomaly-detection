cd542689d2668a1e4ac6ff1cda06a4a22ef1df0f
Krzy-888 <kmacz.888@gmail.com>
2025-10-18 23:50:46 +0200

    removing pickle

--- GUI/pages/1_DTM.py
+++ GUI/pages/1_DTM.py
@@ -17,7 +17,7 @@ st.title("DTM")
 url = "https://github.com/szsendrowski/satellite-space-weather/blob/feature/GUI/GUI/Test_Data/density_map_DOY288_alt525km_Kp4.3.npy"
 response = requests.get(url)
 
-data = np.load(io.BytesIO(response.content), allow_pickle=True)
+data = np.load(io.BytesIO(response.content))
 
 # Coordinates
 height, width = data.shape
