de2b06cbb68253f4137af21615a0974744a4951a
Krzy-888 <kmacz.888@gmail.com>
2025-10-19 09:52:09 +0200

    raw foreign path

--- GUI/GUI_App.py
+++ GUI/GUI_App.py
@@ -4,7 +4,7 @@ import streamlit as st
 def load_css(file_path):
     with open(file_path) as f:
         st.html(f"<style>{f.read()}</style>")
-load_css("https://raw.githubusercontent.com/szsendrowski/satellite-space-weather/feature/GUI/GUI/assets/style.css")
+load_css("https://raw.githubusercontent.com/Krzy-888/MojeMapy/refs/heads/main/style.css")
 
 #Configurations
 st.sidebar.header("HANS")
