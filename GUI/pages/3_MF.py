7142558cc5e1f4c2f22a9e5b9bf475e47ebb513e
Krzy-888 <kmacz.888@gmail.com>
2025-10-19 12:36:34 +0200

    flipping changed

--- GUI/pages/3_MF.py
+++ GUI/pages/3_MF.py
@@ -58,7 +58,6 @@ fig_2 = requests.get('https://raw.githubusercontent.com/Krzy-888/HANS_SRC/main/t
 balistics = Image.open(BytesIO(fig_2.content))
 dat_2 = np.array(balistics)
 dat_2 = np.flipud(dat_2)
-dat_2 = np.fliplr(dat_2)
 # Coordinates
 height, width, _ = dat_2.shape
 x = np.linspace(17.5, 45, width)
@@ -75,7 +74,7 @@ fig_balistics = px.imshow(
 
 fig_balistics.update_layout(
     xaxis_title="Time [h]",
-    yaxis_title="Magnetig Field intensity []",
+    yaxis_title="Magnetig Field intensity [μT]",
     paper_bgcolor='#001d49',
     plot_bgcolor='#001d49',
     title="Total Magnetic Field along ISS"
