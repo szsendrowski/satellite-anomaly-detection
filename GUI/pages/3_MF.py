6d08dfb73beb6515f8c0767fa20a96136c5019a8
Krzy-888 <kmacz.888@gmail.com>
2025-10-19 12:42:10 +0200

    shrinking update

--- GUI/pages/3_MF.py
+++ GUI/pages/3_MF.py
@@ -58,10 +58,11 @@ fig_2 = requests.get('https://raw.githubusercontent.com/Krzy-888/HANS_SRC/main/t
 balistics = Image.open(BytesIO(fig_2.content))
 dat_2 = np.array(balistics)
 dat_2 = np.flipud(dat_2)
+dat_2 = np.fliplr(dat_2)
 # Coordinates
 height, width, _ = dat_2.shape
-x = np.linspace(-0.25, 12.25, width)
-y = np.linspace(17.5, 45, height)
+x = np.linspace(17.5, 45, width)
+y = np.linspace(-0.25, 12.25, height)
 
 
 fig_balistics = px.imshow(
